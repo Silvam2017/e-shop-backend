c33788812720dd7917eb170cbc318bae9181f6b8
Michael Silva <msilva@aggienetwork.com>
2020-06-14 22:48:48 -0500

    added routes and readme

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({}).then(dbShopData => res.json(dbShopData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    include: [{
+      model: Category, Tag
+    }],
+    where: {
+      id: req.params.id
+    }
+  }).then(dbShopData => res.json(dbShopData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
@@ -91,6 +108,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbShopData => {
+    if (!dbShopData[0]) {
+      res.status(404).json({ message: "Invalid ID: No Product found."});
+      return;
+    }
+    res.json(dbShopData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,100 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({})
+  .then(dbShopData => res.json(dbShopData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
 });
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    include: [{
+        model: Product,
+      }],
+    where: {
+      id: req.params.id
+    },
+  }).then(dbShopData => {
+    if (!dbShopData[0]) {
+      res.status(404).json({ message: "Invalid ID: No Tag found."});
+      return;
+    }
+    res.json(dbShopData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    id:req.body.id,
+    tag_name: req.body.tag_name
+  }).then(dbShopData => res.json(dbShopData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  }).then(dbShopData => {
+    if (!dbShopData[0]) {
+      res.status(404).json({ message: "Invalid ID: No Tag found."});
+      return;
+    }
+    res.json(dbShopData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+Tag.destroy({
+  where: {
+    id: req.params.id
+  }
+}).then(dbShopData => {
+  if (!dbShopData[0]) {
+    res.status(404).json({ message: "Invalid ID: No Tag found."});
+    return;
+  }
+  res.json(dbShopData);
+}).catch(err => {
+  console.log(err);
+  res.status(500).json(err);
 });
+});
+
+/* .then(dbShopData => res.json(dbShopData))
+ .catch(err => {
+    console.log(err);
+  res.status(500).json(err);
+
+.then(dbShopData => {
+  if (!dbShopData[0]) {
+    res.status(404).json({ message: "Invalid ID: No Tag found."});
+    return;
+  }
+  res.json(dbShopData);
+}).catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+*/
 
 module.exports = router;
